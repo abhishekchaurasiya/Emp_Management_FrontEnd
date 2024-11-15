3cfec750008a0680bbb43f35053e4450b20c6730
Abhishek Chaurasiya <abhichek2580@gmail.com>
2024-11-15 13:50:10 +0530

    baseurl change

--- src/constants/BaseUrl.jsx
+++ src/constants/BaseUrl.jsx
@@ -1,13 +1,13 @@
 
 export const baseUrl = `https://emp-management-backend-ruby.vercel.app`;
 
-export const authUrl = `${baseUrl}/auth`;
-export const departmentUrl = `${baseUrl}/department`;
-export const employeeUrl = `${baseUrl}/employee`;
-export const leaveUrl = `${baseUrl}/leave`;
-export const salaryUrl = `${baseUrl}/salary`;
-export const settingUrl = `${baseUrl}/setting`;
-export const dashboardUrl = `${baseUrl}/dashboard`;
+export const authUrl = `${baseUrl}/dev/api/v1/auth`;
+export const departmentUrl = `${baseUrl}/dev/api/v1/department`;
+export const employeeUrl = `${baseUrl}/dev/api/v1/employee`;
+export const leaveUrl = `${baseUrl}/dev/api/v1/leave`;
+export const salaryUrl = `${baseUrl}/dev/api/v1/salary`;
+export const settingUrl = `${baseUrl}/dev/api/v1/setting`;
+export const dashboardUrl = `${baseUrl}dev/api/v1/dashboard`;
 
 
 
